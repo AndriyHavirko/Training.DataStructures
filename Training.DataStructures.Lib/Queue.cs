d3d065e0092304e745e7918e63560ad0d9a1a246
Andriy Havirko <a.havirko@gmail.com>
2014-06-22 23:00:10 +0300

    draft Queue

---
+++ Training.DataStructures.Lib/Queue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.DataStructures.Lib
+{
+    class Queue<T>
+    {
+        private LinkedListNode<T> first;
+        private LinkedListNode<T> last;
+
+        public Queue()
+        {
+            this.first = null;
+            this.last = this.first;
+            this.Count = 0;
+        }
+
+        public int Count
+        {
+            get;
+            private set;
+        }
+
+        public bool Contains(T item)
+        {
+            // implementation here
+            return false;
+        }
+
+        public void Clear()
+        {
+ 
+        }
+
+        public T Dequeue()
+        {
+            // implementation
+            return default(T);
+        }
+
+        public void Enqueue(T item)
+        {
+ 
+        }
+
+        public void Enqueue(IEnumerable<T> items)
+        {
+ 
+        }
+    }
+}
