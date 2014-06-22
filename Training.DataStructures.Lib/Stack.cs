c68e450cee02584f8b8ef254039afc2472daefd2
Andriy Havirko <a.havirko@gmail.com>
2014-06-22 22:34:59 +0300

    draft Stack

---
+++ Training.DataStructures.Lib/Stack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Training.DataStructures.Lib
+{
+    class Stack<T>
+    {
+        private LinkedListNode<T> top;
+
+        public Stack()
+        {
+            this.top = null;
+            this.Count = 0;
+        }
+
+        public int Count
+        {
+            get;
+            private set;
+        }
+
+        public void Clear()
+        {
+ 
+        }
+
+        public bool Contains(T item)
+        {
+            // TODO: add an implementation
+            return false;
+        }
+
+        public T Pop()
+        {
+            // TODO: add an implementation
+            return default(T);
+        }
+
+        public void Push(T item)
+        {
+
+        }
+
+        public void Push(IEnumerable<T> items)
+        {
+ 
+        }
+    }
+}
