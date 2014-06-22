2381ab681fd90a4b181e9694a71eba18d0801c51
Andriy Havirko <a.havirko@gmail.com>
2014-06-22 14:23:05 +0300

    added LinkedListNode class

---
+++ Training.DataStructures.Lib/LinkedListNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Training.DataStructures.Lib
+{
+    public class LinkedListNode<T>: IComparable<T> where T: IComparable<T>
+    {
+        private T data;
+        private LinkedListNode<T> next;
+
+        public LinkedListNode()
+        {
+            data = default(T);
+            next = null;
+        }
+
+        public LinkedListNode(T data, LinkedListNode<T> next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+
+        public T Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public LinkedListNode<T> Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+
+        public int CompareTo(T other)
+        {
+            return data.CompareTo(other);
+        }
+    }
+}
