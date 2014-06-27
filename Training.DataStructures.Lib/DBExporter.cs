1efd41be53c09be372f324528ebfb1e985199adc
Andriy Havirko <andriy.havirko@eleks.com>
2014-06-27 14:13:17 +0300

    Updated methods SavaToDb and ReadFromDb

--- Training.DataStructures.Lib/DBExporter.cs
+++ Training.DataStructures.Lib/DBExporter.cs
@@ -1,58 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 
 namespace Training.DataStructures.Lib
 {
-    public static class DBExporter
+    public static class DbExporter
     {
-
-        public static void SaveToDB(LinkedList<String> data)
+        public static void SaveLinkeListToDb<T>(LinkedList<T> list, String connectionString) 
+            where T : IComparable<T>, IEquatable<T>
         {
-
-            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+            if (list == null) 
+                throw new ArgumentNullException();
+            using (var connection = new SqlConnection(connectionString))
             {
+                var command = new SqlCommand("INSERT INTO Data(Data, Previous, Next) VALUES (@data, @previous, @next); select @@IDENTITY", connection);
+                command.Parameters.Add(new SqlParameter("data", SqlDbType.NVarChar));
+                command.Parameters.Add(new SqlParameter("previous", SqlDbType.Int));
+                command.Parameters.Add(new SqlParameter("next", SqlDbType.Int));
+
+                int previousItemId = 0;
+                int nextItemId = 0;
+
                 connection.Open();
-                foreach (var d in data)
+                foreach (var item in list)
                 {
-                    var command = connection.CreateCommand();
-                    command.CommandText = "INSERT INTO Data(data) VALUES (@data); select @@IDENTITY";
-                    //var param = command.CreateParameter();
-                    SqlParameter p = new SqlParameter("data", d.ToString());
-                    command.Parameters.Add(p);
-
-
-                    var obj = command.ExecuteScalar();
-                    //conn.Close();
+                    command.Parameters["data"].Value = item;
+                    command.Parameters["previous"].Value = previousItemId;
+                    command.Parameters["next"].Value = nextItemId;
+                    previousItemId = (int)(decimal)command.ExecuteScalar();
                 }
-
             }
-
         }
 
-        public static void ReadFromDB(LinkedList<String> data)
+        //public static void ReadLinkedListFromDb(LinkedList<String> list)
+        //{
+        //    using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+        //    {
+        //        var dataSet = new DataSet();
+        //        var adapter = new SqlDataAdapter("SELECT * FROM Data", connection);
+        //        adapter.Fill(dataSet);
+
+        //        foreach (var row in dataSet.Tables["Data"].Rows)
+        //        {
+                    
+        //        }
+        //    }
+        //}
+
+        public static void SaveToDbFromCollection<T>(ICollection<T> collection, String connectionString)
         {
-            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString))
+            if (collection == null) 
+                throw new ArgumentNullException();
+            using (var connection = new SqlConnection(connectionString))
             {
+                var command = new SqlCommand("INSERT INTO Data(data) VALUES (@data);", connection);
+                command.Parameters.Add(new SqlParameter("data", SqlDbType.NVarChar));
+
                 connection.Open();
-                SqlCommand command = new SqlCommand("SELECT * FROM data", conn);
-                using (var reader = command.ExecuteReader())
+                foreach (var item in collection)
                 {
-                    while (reader.Read())
-                    {
-                        int id = reader.GetInt32(reader.GetOrdinal("ID"));
-                    }
+                    command.Parameters["data"].Value = item.ToString();
+                    command.ExecuteNonQuery();
                 }
+            }
+        }
 
-                DataSet set = new DataSet();
-                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM data", conn);
-                //adapter.upFill(set);
-
-                foreach (DataRow row in set.Tables[0].Rows)
+        public static void ReadFromDbIntoCollection<T>(ICollection<T> collection, String connectionString)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var dataSet = new DataSet();
+                var adapter = new SqlDataAdapter("SELECT * FROM Data", connection);
+                adapter.Fill(dataSet);
+                foreach (DataRow row in dataSet.Tables[0].Rows)
                 {
-                    var x = (int)row["ID"];
+                    collection.Add((T)row["Data"]);
                 }
             }
         }
