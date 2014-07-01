2969248aae231cc75457ac004ff04da485ec32b5
Andriy Havirko <andriy.havirko@eleks.com>
2014-07-01 13:07:14 +0300

    Moved DBExporter class to App project

--- Training.DataStructures.App/DBExporter.cs
+++ Training.DataStructures.App/DBExporter.cs
@@ -5,7 +5,7 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace Training.DataStructures.Lib
+namespace Training.DataStructures.App
 {
     public static class DbExporter
     {
