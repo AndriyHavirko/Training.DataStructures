15502a6f22fdd55e628dc308b88f07e07351fb2d
Andriy Havirko <andriy.havirko@eleks.com>
2014-07-01 20:10:09 +0300

    Added empty tests for other data structures

---
+++ Training.DataStructures.Tests/ArrayListTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training.DataStructures.Tests
+{
+    [TestClass]
+    public class ArrayListTests
+    {
+        
+    }
+}
