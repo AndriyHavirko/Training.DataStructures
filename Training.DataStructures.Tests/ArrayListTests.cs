8559dc861e4642da6d0a2fb5b4f0cda2ca5dd4eb
Andriy Havirko <andriy.havirko@eleks.com>
2014-07-02 18:39:17 +0300

    Updated some tests

--- Training.DataStructures.Tests/ArrayListTests.cs
+++ Training.DataStructures.Tests/ArrayListTests.cs
@@ -1,11 +1,107 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 namespace Training.DataStructures.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class ArrayListTests
     {
-        
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(100000)]
+        public void CareateAnEmptyArrayList(int capacity)
+        {
+            ArrayList<String> list;
+            if (capacity == 0)
+                list = new ArrayList<String>();
+            else
+                list = new ArrayList<String>(capacity);
+                
+            Assert.IsNotNull(list);
+            Assert.AreEqual(list.Count, 0);
+            Assert.IsTrue(list.Capacity >= capacity);
+        }
+
+        [Test]
+        public void CreateAnEmptyArrayList_NegativeCapacity()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ArrayList<String>(-1));
+        }
+
+        [Test]
+        public void Contains_CheckIfFalse_OnEmptyArrayList()
+        {
+            var list = new ArrayList<String>();
+            Assert.IsFalse(list.Contains(String.Empty));
+            Assert.IsFalse(list.Contains("Test"));
+        }
+
+        [Test]
+        public void Contains_CheckIfFalse()
+        {
+            var list = new ArrayList<String>() { "Test 1", "Test 2" };
+            Assert.IsFalse(list.Contains(String.Empty));
+            Assert.IsFalse(list.Contains("Test 3"));
+        }
+
+        [Test]
+        public void Contains_CheckIfTrue()
+        {
+            var list = new ArrayList<String>() { String.Empty, "Test 1", "Test 2" };
+            Assert.IsTrue(list.Contains(String.Empty));
+            Assert.IsTrue(list.Contains("Test 2"));
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(100000)]
+        public void Add_MultibleElementsToArrayList(int count)
+        {
+            var list = new ArrayList<String>();
+            var random = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(String.Format("Test {0}", random.Next()));
+            }
+            Assert.AreEqual(list.Count, count);
+            Assert.IsTrue(list.Capacity >= count);
+        }
+
+        [Test]
+        public void Clear()
+        {
+            var list = new ArrayList<int>(100);
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            list.Clear();
+
+            Assert.AreEqual(list.Count, 0);
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(100000000)]
+        public void Capacity(int capacity)
+        {
+            var list = new ArrayList<int>();
+            list.Capacity = capacity;
+            Assert.AreEqual(list.Capacity, capacity);
+        }
+
+        [TestCase(Int32.MinValue)]
+        [TestCase(-1)]
+        public void Capacity_LessThanZero(int capacity)
+        {
+            var list = new ArrayList<String>();
+            list.Capacity = capacity;
+            Assert.IsTrue(list.Capacity >= 0);
+        }
+
+
     }
 }
--- Training.DataStructures.Tests/StackTests.cs
+++ Training.DataStructures.Tests/StackTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 //using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Training.DataStructures.Tests
@@ -11,12 +13,13 @@ namespace Training.DataStructures.Tests
         
         private String[] arrayOfStrings;
 
+        private const int maxSize = 1000000;
 
         [SetUp]
         public void SetUp()
         {
             var random = new Random();
-            var size = 100;
+            var size = maxSize;
 
             stackOfStrings = new Stack<string>();
             
@@ -28,14 +31,14 @@ namespace Training.DataStructures.Tests
             }
         }
 
-        [TestCase]
+        [Test]
         public void Emty()
         {
             Assert.AreEqual(stackOfStrings.Count, 0);
             Assert.Throws<InvalidOperationException>(() => stackOfStrings.Pop());
         }
 
-        [TestCase]
+        [Test]
         public void Contains()
         {
             Assert.IsFalse(stackOfStrings.Contains("Test 1"));
@@ -51,8 +54,7 @@ namespace Training.DataStructures.Tests
 
         [TestCase(0)]
         [TestCase(1)]
-        [TestCase(10)]
-        [TestCase(100)]
+        [TestCase(maxSize)]
         public void Push(int size)
         {
             for (int i = 0; i<size; i++)
@@ -62,19 +64,17 @@ namespace Training.DataStructures.Tests
             }
             Assert.AreEqual(stackOfStrings.Count, size);
         }
-
+        
+        [Test]
         public void PushCollection()
         {
             stackOfStrings.Push(arrayOfStrings);
             Assert.AreEqual(stackOfStrings.Count, arrayOfStrings.Length);
-            foreach (var s in arrayOfStrings)
-                Assert.IsTrue(stackOfStrings.Contains(s));
         }
 
         [TestCase(0)]
         [TestCase(1)]
-        [TestCase(10)]
-        [TestCase(100)]
+        [TestCase(maxSize)]
         public void Pop(int size)
         {
             for (int i = 0; i < size; i++)
@@ -88,7 +88,7 @@ namespace Training.DataStructures.Tests
             }
         }
 
-        [TestCase]
+        [Test]
         public void Clear()
         {
             stackOfStrings.Push(arrayOfStrings);
@@ -98,5 +98,33 @@ namespace Training.DataStructures.Tests
             Assert.AreEqual(stackOfStrings.Count, 0);
             Assert.IsNull(stackOfStrings.Top);
         }
+
+        [Test]
+        public void IsSynhronized()
+        {
+            Assert.IsFalse(((ICollection)stackOfStrings).IsSynchronized);
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(maxSize)]
+        public void CopyTo(int size)
+        {
+            var destinationArray = new string[size];
+
+            var anotherArray = new string[size];
+            Array.Copy(arrayOfStrings, 0, anotherArray, 0, size);
+
+            for (int i = 0; i < size; i++)
+            {
+                stackOfStrings.Push(arrayOfStrings[i]);
+                Assert.AreEqual(stackOfStrings.Top.Data, arrayOfStrings[i]);
+            }
+
+            stackOfStrings.CopyTo(destinationArray, 0);
+
+            CollectionAssert.AreEqual(destinationArray, anotherArray.Reverse());
+        }
+
     }
 }
