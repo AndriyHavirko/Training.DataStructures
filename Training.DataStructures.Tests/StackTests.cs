0736449204c4dba40d4b12db297ec00b6669f5f5
Andriy Havirko <andriy.havirko@eleks.com>
2014-07-02 14:37:11 +0300

    Revert "Renamed StackTests"
    This reverts commit 8502fff944e3f015899ae497d952c503644d11a6.

--- Training.DataStructures.Tests/StackTests.cs
+++ Training.DataStructures.Tests/StackTests.cs
@@ -5,7 +5,7 @@ using NUnit.Framework;
 namespace Training.DataStructures.Tests
 {
     [TestFixture]
-    public class StackNUnitTests
+    public class StackTests
     {
         private Stack<String> stackOfStrings;
         
@@ -28,14 +28,14 @@ namespace Training.DataStructures.Tests
             }
         }
 
-        [Test]
+        [TestCase]
         public void Emty()
         {
             Assert.AreEqual(stackOfStrings.Count, 0);
             Assert.Throws<InvalidOperationException>(() => stackOfStrings.Pop());
         }
 
-        [Test]
+        [TestCase]
         public void Contains()
         {
             Assert.IsFalse(stackOfStrings.Contains("Test 1"));
@@ -88,7 +88,7 @@ namespace Training.DataStructures.Tests
             }
         }
 
-        [Test]
+        [TestCase]
         public void Clear()
         {
             stackOfStrings.Push(arrayOfStrings);
